44f905559237ffcc23d4e0549d0fae505acc537d
jogapps <jogappsdeveloper@gmail.com>
2021-08-14 11:04:28 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,69 @@
+  /*=====================================
+    Sticky
+    ======================================= */
+    window.onscroll = function () {
+        var header_navbar = document.querySelector(".navbar-area");
+        var sticky = header_navbar.offsetTop;
+        const search = document.getElementById('search')
+        const acct_btn = document.getElementById('acct_btn')
+
+        var logo = document.querySelector('.navbar-brand img')
+        if (window.pageYOffset > sticky) {
+          header_navbar.classList.add("sticky");
+          logo.src = 'assets/images/logo2.png';
+          search.style.color='#051441'
+        } else {
+          header_navbar.classList.remove("sticky");
+          logo.src = 'assets/images/logo.png';
+          search.style.color='white'
+    
+        }
+
+        // show or hide the back-top-top button
+        var backToTo = document.querySelector(".scroll-top");
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            backToTo.style.display = "flex";
+        } else {
+            backToTo.style.display = "none";
+        }
+    };
+
+    //===== mobile-menu-btn
+    let navbarToggler = document.querySelector(".mobile-menu-btn");
+    navbarToggler.addEventListener('click', function () {
+        navbarToggler.classList.toggle("active");
+    });
+
+
+    var counted = 0;
+$(window).scroll(function() {
+
+  var oTop = $('#counter').offset().top - window.innerHeight;
+  if (counted == 0 && $(window).scrollTop() > oTop) {
+    $('.count').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
+
+        {
+
+          duration: 2000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+
+        });
+    });
+    counted = 1;
+  }
+
+});
